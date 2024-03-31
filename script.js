63c704065e8d5846c2aef9b542a536d52309c36b
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-03-31 17:13:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+   // Retrieve the values of the First and Last name from the form
+    const firstName = document.getElementsByName('fname')[0].value.trim();
+    const lastName = document.getElementsByName('lname')[0].value.trim();
 
+    // Concatenate the First and Last name
+    const fullName = firstName + ' ' + lastName;
+
+    // Display the First and Last name using an alert
+    alert(fullName);
 }
